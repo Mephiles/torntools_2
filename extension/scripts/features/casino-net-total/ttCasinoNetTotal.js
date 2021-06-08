2a4402be28f09a54ce47eccf320c7df4f2357b0f
DeKleineKobini <kobedepeuter@outlook.com>
2021-06-08 20:12:51 +0200

    General fixes.

--- extension/scripts/features/casino-net-total/ttCasinoNetTotal.js
+++ extension/scripts/features/casino-net-total/ttCasinoNetTotal.js
@@ -1,6 +1,17 @@
 "use strict";
 
 (async () => {
+	console.log("DKK CASINO");
+	const page = getPage();
+
+	if (page === "loader") {
+		const sid = getSearchParameters().get("sid");
+
+		if (["viewLotteryStats"].includes(sid)) {
+			return;
+		}
+	}
+
 	const feature = featureManager.registerFeature(
 		"Casino Net Total",
 		"casino",
@@ -17,7 +28,7 @@
 	function initialiseListener() {
 		if (window.location.href.includes("bookies")) {
 			window.addEventListener("hashchange", () => {
-				if (feature.enabled() && !window.location.href.includes("Lottery") && window.location.href.includes("stats/")) {
+				if (feature.enabled() && window.location.hash.includes("stats/")) {
 					addTotal();
 				}
 			});
@@ -25,11 +36,12 @@
 	}
 
 	async function addTotal() {
-		if (!window.location.href.includes("Lottery") && (window.location.href.includes("Statistics") || window.location.href.includes("Stats") || window.location.href.includes("stats"))) {
-			for (const statsType of ["overall", "your"]) {
-				await requireElement(`#${statsType}-stats .stat-value`);
-				const moneyElementsList = document.evaluate(
-					`//div[contains(@id,"${statsType}-stats")]
+		if (page === "bookies" && !window.location.hash.includes("stats/")) return;
+
+		for (const statsType of ["overall", "your"]) {
+			await requireElement(`#${statsType}-stats .stat-value`);
+			const moneyElementsList = document.evaluate(
+				`//div[contains(@id,"${statsType}-stats")]
 					//li
 						[
 							(contains(text(), "Total money won") or contains(text(), "Total money gain"))
@@ -37,30 +49,30 @@
 							(contains(text(), "Total money lost") or contains(text(), "Total money loss"))
 						]
 							/following-sibling::li[(contains(@class, "stat-value"))]`,
-					document,
-					null,
-					XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-					null
-				);
-				if (moneyElementsList.snapshotLength !== 2) continue;
-				const totalWon = parseInt(moneyElementsList.snapshotItem(0).innerText.replace(/[\$\, ]/g, ""));
-				const totalLostElement = moneyElementsList.snapshotItem(1);
-				const totalLost = parseInt(totalLostElement.innerText.replace(/[\$\, ]/g, ""));
+				document,
+				null,
+				XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+				null
+			);
+			if (moneyElementsList.snapshotLength !== 2) continue;
+			const totalWon = parseInt(moneyElementsList.snapshotItem(0).innerText.replace(/[$, ]/g, ""));
+			const totalLostElement = moneyElementsList.snapshotItem(1);
+			const totalLost = parseInt(totalLostElement.innerText.replace(/[$, ]/g, ""));
 
-				if (document.find(`.${statsType}-stats-wrap .tt-net-total`) || (window.location.toString().includes("Poker") && statsType === "overall")) return;
-				await requireElement(`.stats-wrap .${statsType}-stats-wrap .stat`);
-				totalLostElement.closest("li:not(.stat-value)").insertAdjacentHTML(
-					"afterEnd",
-					`<div class="tt-net-total ${window.location.href.includes("bookie") ? "bookie" : ""}">
+			if (document.find(`.${statsType}-stats-wrap .tt-net-total`) || (window.location.toString().includes("Poker") && statsType === "overall")) return;
+
+			await requireElement(`.stats-wrap .${statsType}-stats-wrap .stat`);
+			totalLostElement.closest("li:not(.stat-value)").insertAdjacentHTML(
+				"afterend",
+				`<div class="tt-net-total ${window.location.href.includes("bookie") ? "bookie" : ""}">
 						<li class="name">Net total</li>
 						<li class="value">${formatNumber(totalWon - totalLost, { currency: true })}</li>
 					</div>`
-				);
-			}
+			);
 		}
 	}
 
 	function removeTotal() {
-		document.findAll(".tt-net-total").forEach(x => x.remove());
+		document.findAll(".tt-net-total").forEach((x) => x.remove());
 	}
 })();
