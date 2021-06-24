00f774d94276ed28e24f7293982b20f2f68e09c0
DeKleineKobini <kobedepeuter@outlook.com>
2021-06-24 22:40:44 +0200

    Start actually loading data.

--- extension/scripts/features/gym-graph/ttGymGraph.js
+++ extension/scripts/features/gym-graph/ttGymGraph.js
@@ -26,9 +26,64 @@
 
 		showLoadingPlaceholder(content, true);
 
-		await sleep(30000);
+		addButton();
+		await loadGraph();
 
 		showLoadingPlaceholder(content, false);
+
+		function addButton() {
+			const wrapper = document.newElement({ type: "div", class: "tornstats-update" });
+
+			const button = document.newElement({ type: "button", text: "Update TornStats", class: "tt-btn tornstats-button" });
+
+			button.addEventListener("click", async () => {
+				if (wrapper.find(".tornstats-response")) wrapper.find(".tornstats-response").remove();
+
+				const responseElement = document.newElement({ type: "div", class: "tornstats-response" });
+				wrapper.appendChild(responseElement);
+
+				button.setAttribute("disabled", "");
+				showLoadingPlaceholder(responseElement, true);
+
+				const { message, status } = await fetchData("tornstats", { section: "battlestats/record" })
+					.then((response) => {
+						// FIXME - Improve message.
+						console.log("DKK TS", response);
+
+						return response;
+					})
+					.catch((error) => ({ message: error, status: false }));
+
+				responseElement.innerText = message;
+				responseElement.classList.add(status ? "success" : "error");
+
+				button.removeAttribute("disabled");
+			});
+
+			wrapper.appendChild(button);
+			content.appendChild(wrapper);
+		}
+
+		async function loadGraph() {
+			let result;
+			if (ttCache.hasValue("gym", "graph")) {
+				result = ttCache.get("gym", "graph");
+			} else {
+				try {
+					result = await fetchData("tornstats", { section: "battlestats/graph" });
+
+					if (result.status) {
+						ttCache.set({ graph: result }, TO_MILLIS.HOURS, "gym").then(() => {});
+					}
+				} catch (error) {
+					console.log("TT - Failed to load graph from TornStats.", error);
+					return;
+				}
+			}
+
+			// FIXME - Show graph.
+			console.log("DKK loadGraph", result);
+		}
 	}
 
 	function removeGraph() {
