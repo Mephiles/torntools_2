165baf1401ec750fd36e2269c0d4b01a7e23adf8
DeKleineKobini <kobedepeuter@outlook.com>
2021-07-17 12:19:36 +0200

    Add more achievements.

--- extension/scripts/features/achievements/achievements.js
+++ extension/scripts/features/achievements/achievements.js
@@ -17,22 +17,19 @@ const ACHIEVEMENTS = [
 		name: "Awards",
 		stats: () => userdata.personalstats.awards,
 		detection: { keyword: "total awards" },
-		// FIXME - Load on the awards page?
-		requirements: { pages: ["home"] },
+		requirements: { pages: ["home", "awards"] },
 	},
 	{
 		name: "Married (days)",
 		stats: () => userdata.married.duration,
 		detection: { keyword: "stay married" },
-		// FIXME - Load on the church?
-		requirements: { pages: ["home"] },
+		requirements: { pages: ["home", "church"] },
 	},
 	{
 		name: "Points sold",
 		stats: () => userdata.personalstats.pointssold,
 		detection: { keyword: "points on the market" },
-		// FIXME - Load on the points market?
-		requirements: { pages: ["home"] },
+		requirements: { pages: ["home", "pmarket"] },
 	},
 	{
 		name: "Activity",
@@ -90,5 +87,119 @@ const ACHIEVEMENTS = [
 		stats: () => userdata.personalstats.networth,
 		detection: { keyword: "networth" },
 		requirements: { pages: ["home"] },
+	},
+	{
+		name: "Bounties collected",
+		stats: () => userdata.personalstats.bountiescollected,
+		detection: { keyword: "bounties", include: ["collect"] },
+		requirements: { pages: ["bounties"] },
+	},
+	{
+		name: "Bounties collected (money)",
+		stats: () => userdata.personalstats.totalbountyreward,
+		detection: { keyword: "bounty", include: ["earn", "hunting"] },
+		requirements: { pages: ["bounties"] },
+	},
+	{
+		name: "Donations",
+		stats: () => {
+			const description = document.find("#church-donate .desc > p:first-child > span");
+			if (!description) return -1;
+
+			return parseInt(description.innerText.substring(1).replaceAll(",", ""));
+		},
+		detection: { keyword: "church" },
+		requirements: { pages: ["church"] },
+	},
+	// FIXME - Fix these.
+	{
+		name: "City finds",
+		stats: () => userdata.personalstats.cityfinds,
+		detection: { keyword: "city", include: ["find", "items"] },
+		requirements: { pages: ["city"] },
+	},
+	{
+		name: "Dump finds",
+		stats: () => userdata.personalstats.dumpfinds,
+		detection: { keyword: "dump" },
+		requirements: { pages: ["dump"] },
+	},
+	{
+		name: "Complete courses",
+		stats: () => userdata.education_completed.length,
+		detection: { keyword: "education courses" },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Biology Bachelor",
+		stats: () => (userdata.education_completed.includes(42) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Business Bachelor",
+		stats: () => (userdata.education_completed.includes(13) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Combat Bachelor",
+		stats: () => (userdata.education_completed.includes(87) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "ICT Bachelor",
+		stats: () => (userdata.education_completed.includes(62) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "General Bachelor",
+		stats: () => (userdata.education_completed.includes(121) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Fitness Bachelor",
+		stats: () => (userdata.education_completed.includes(111) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "History Bachelor",
+		stats: () => (userdata.education_completed.includes(21) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Law Bachelor",
+		stats: () => (userdata.education_completed.includes(102) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Mathematics Bachelor",
+		stats: () => (userdata.education_completed.includes(33) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Psychology Bachelor",
+		stats: () => (userdata.education_completed.includes(69) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Defense Bachelor",
+		stats: () => (userdata.education_completed.includes(42) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
+	},
+	{
+		name: "Sports Bachelor",
+		stats: () => (userdata.education_completed.includes(51) ? 1 : 0),
+		detection: { goals: [1] },
+		requirements: { pages: ["education"] },
 	},
 ];
