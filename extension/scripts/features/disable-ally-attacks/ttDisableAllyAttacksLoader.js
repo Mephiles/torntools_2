2b6a5f33e6916f756a5ad37682638b09a640d8b8
Sashank999 <bandi.rao999@gmail.com>
2021-07-18 11:07:42 +0530

    Complete

--- extension/scripts/features/disable-ally-attacks/ttDisableAllyAttacksLoader.js
+++ extension/scripts/features/disable-ally-attacks/ttDisableAllyAttacksLoader.js
@@ -1,62 +1,55 @@
 "use strict";
 
 (async () => {
-	if (!getPageStatus().access) return;
-
+	startListener();
 	const feature = featureManager.registerFeature(
 		"Disable Ally Attacks",
 		"loader",
 		() => settings.pages.profile.disableAllyAttacks,
 		startListener,
 		disableAttackButton,
-		() => {},
+		removeWarning,
 		{
 			storage: ["settings.pages.profile.disableAllyAttacks", "settings.allyFactionsIDs"],
 		},
 		null
 	);
 
+	let closedOption = false;
 	async function startListener() {
 		addFetchListener(({ detail: { page, json, fetch } }) => {
-			if (page !== "loader" || !json || !json.DB || !json.DB.defenderUser || !json.DB.defenderUser.factionID) return;
+			if (closedOption || !feature.enabled() || page !== "loader" || !json || !json.DB || !json.DB.defenderUser || !json.DB.defenderUser.factionID) return;
 			disableAttackButton(json.DB.defenderUser.factionID);
 		});
 	}
 
-	let closedOption = false;
-	function disableAttackButton(factionID) {
-		if (document.find(".tt-disable-ally-attack") || closedOption) return;
+	async function disableAttackButton(factionID) {
+		if (document.find(".tt-disable-ally-attack")) return;
+		await requireElement("[class*='players__'] #defender [class*='modal__']");
 		if (
-			!closedOption &&
 			!isNaN(factionID) &&
 			(hasAPIData() &&
 			factionID === userdata.faction.faction_id) ||
 			settings.allyFactionsIDs.some((ally) => ally === factionID || ally.toString() === factionID)
 		) {
-			const coordinates = document.find("[class*='players__'] #defender [class*='modal__']").getBoundingClientRect();
-			const stopperDiv = document.newElement({
+			const warningDiv = document.newElement({
 				type: "div",
 				class: "tt-disable-ally-attack",
-				text: "This player is an ally. Click here if you are sure to attack.",
-				attributes: {
-					style: `
-						left: ${coordinates.left}px;
-						top: ${coordinates.top}px;
-						position: absolute;
-						background-color: #999;
-						z-index: 100000;
-						width: ${coordinates.width}px;
-						height: ${coordinates.height}px;
-					`,
-				},
+				text: "Blocked by TornTools. This player is an ally. Click here if you are sure to attack.",
 			});
-			document.body.appendChild(stopperDiv);
-			stopperDiv.addEventListener("click", (event) => {
-				closedOption = true;
+			document.find("[class*='players__'] #defender [class*='modal__']").insertAdjacentElement("afterbegin", warningDiv);
+			warningDiv.addEventListener("click", (event) => {
 				event.preventDefault();
 				event.stopImmediatePropagation();
-				if (confirm("Are you sure you want to attack this ally ?")) event.target.remove();
+				if (confirm("Are you sure you want to attack this ally ?")) {
+					event.target.remove();
+					closedOption = true;
+				};
 			});
 		}
+	}
+
+	function removeWarning() {
+		document.findAll(".tt-disable-ally-attack").forEach(x => x.remove());
 	}
 })();
