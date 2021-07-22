f2037b9a01fd099ac8066fbea7763b493c347130
Sashank999 <bandi.rao999@gmail.com>
2021-07-22 11:16:24 +0530

    Flying Time (Time taken to fly and return)

--- extension/scripts/content/travelagency/ttTravelAgency.js
+++ extension/scripts/content/travelagency/ttTravelAgency.js
@@ -1,24 +1,41 @@
 "use strict";
 
-(() => {
-	for (const type of document.findAll(".travel-agency .torn-tabs > li:not([data-state='disabled'])")) {
-		type.addEventListener("click", async () => {
-			triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_TYPE, { type: type.find(".travel-name").innerText.toLowerCase() });
+(async () => {
+	if (await checkMobile() || await checkTablet()) {
+		document.find(".travel-agency").addEventListener("click", () => {
+			if (
+				event.target.closest(".travel-info-table-list[aria-selected*='true'][role*='tab']")
+			) {
+				triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_COUNTRY, null);
+			}
 		});
-	}
-	for (const flag of document.findAll(".travel-agency .raceway")) {
-		flag.addEventListener("click", async () => {
-			const container = document.find(".travel-agency > div[aria-expanded='true'] .travel-container.full-map[style='display: block;']");
-			if (container) {
-				await new Promise((resolve) => {
-					new MutationObserver((mutations, observer) => {
-						observer.disconnect();
-						resolve();
-					}).observe(container, { childList: true });
-				});
-			} else await requireElement(".travel-agency > div[aria-expanded='true'] .travel-container.full-map[style='display: block;']");
+		document.find(".torn-tabs").addEventListener("click", () => {
+			if (
+				event.target.closest(".torn-tabs li[aria-selected='true']")
+			) {
+				triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_TYPE, null);
+			}
+		});
+	} else {
+		for (const type of document.findAll(".travel-agency .torn-tabs > li:not([data-state='disabled'])")) {
+			type.addEventListener("click", async () => {
+				triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_TYPE, { type: type.find(".travel-name").innerText.toLowerCase() });
+			});
+		}
+		for (const flag of document.findAll(".travel-agency .raceway")) {
+			flag.addEventListener("click", async () => {
+				const container = document.find(".travel-agency > div[aria-expanded='true'] .travel-container.full-map[style='display: block;']");
+				if (container) {
+					await new Promise((resolve) => {
+						new MutationObserver((mutations, observer) => {
+							observer.disconnect();
+							resolve();
+						}).observe(container, { childList: true });
+					});
+				} else await requireElement(".travel-agency > div[aria-expanded='true'] .travel-container.full-map[style='display: block;']");
 
-			triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_COUNTRY, { country: flag.dataset.race });
-		});
+				triggerCustomListener(EVENT_CHANNELS.TRAVEL_SELECT_COUNTRY, { country: flag.dataset.race });
+			});
+		}
 	}
 })();
