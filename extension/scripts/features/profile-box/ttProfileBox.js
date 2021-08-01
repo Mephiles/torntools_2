ee0c3b1f946e13dded9c065cfc6eff32a6e205c4
DeKleineKobini <kobedepeuter@outlook.com>
2021-08-01 20:48:36 +0200

    Position parts.

--- extension/scripts/features/profile-box/ttProfileBox.js
+++ extension/scripts/features/profile-box/ttProfileBox.js
@@ -26,6 +26,27 @@
 			nextElement: document.find(".medals-wrapper") || document.find(".basic-information")?.closest(".profile-wrapper"),
 			class: "mt10",
 		});
+
+		buildStats().catch((error) => console.log("TT - Couldn't build the stats part of the profile box.", error));
+		buildSpy().catch((error) => console.log("TT - Couldn't build the spy part of the profile box.", error));
+		buildStakeouts().catch((error) => console.log("TT - Couldn't build the stakeout part of the profile box.", error));
+		buildAttackHistory().catch((error) => console.log("TT - Couldn't build the attack history part of the profile box.", error));
+
+		async function buildStats() {
+			content.appendChild(document.newElement({ type: "div", class: "user-stats", text: "Stats" }));
+		}
+
+		async function buildSpy() {
+			content.appendChild(document.newElement({ type: "div", class: "spy-information", text: "Spy" }));
+		}
+
+		async function buildStakeouts() {
+			content.appendChild(document.newElement({ type: "div", class: "stakeout", text: "Stakeout" }));
+		}
+
+		async function buildAttackHistory() {
+			content.appendChild(document.newElement({ type: "div", class: "attack-history", text: "Attack History" }));
+		}
 	}
 
 	function removeBox() {
