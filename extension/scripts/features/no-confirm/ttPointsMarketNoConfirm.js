a2a2cea9bef1750217a072711a063cdde79e2cb1
DeKleineKobini <kobedepeuter@outlook.com>
2021-09-18 17:47:34 +0200

    Remove confirmation for removing points.

--- extension/scripts/features/no-confirm/ttPointsMarketNoConfirm.js
+++ extension/scripts/features/no-confirm/ttPointsMarketNoConfirm.js
@@ -1,12 +1,6 @@
 "use strict";
 
 (async () => {
-	const VIEW_TYPES = {
-		UNKNOWN: "unknown",
-		BROWSE: "browse_view",
-		ITEM: "item_view",
-	};
-
 	const feature = featureManager.registerFeature(
 		"PointsMarket No Confirm",
 		"no confirm",
@@ -19,56 +13,25 @@
 	);
 
 	function initialise() {
-		// new MutationObserver(async (mutations) => {
-		// 	if (!feature.enabled()) return;
-		//
-		// 	if (getViewType() === VIEW_TYPES.UNKNOWN) return;
-		//
-		// 	// Filter out changes where the main body doesn't get added.
-		// 	if (
-		// 		![...[...mutations].find((mutation) => mutation.addedNodes.length).addedNodes]
-		// 			.filter((node) => node.nodeType === Node.ELEMENT_NODE)
-		// 			.some((node) => node.classList.contains("main-market-page") || node.classList.contains("shop-market-page"))
-		// 	)
-		// 		return;
-		//
-		// 	startFeature();
-		// }).observe(document.find("#item-market-main-wrap"), { childList: true });
+		new MutationObserver((mutations) => {
+			if (!feature.enabled()) return;
+
+			if (mutations[0].removedNodes.length > 1) return;
+
+			startFeature();
+		}).observe(document.find(".users-point-sell"), { childList: true });
 	}
 
-	function startFeature() {
-		// switch (getViewType()) {
-		// 	case VIEW_TYPES.ITEM:
-		// 		requireElement(".buy .buy-link").then(() => removeConfirmation());
-		// 		break;
-		// 	case VIEW_TYPES.BROWSE:
-		// 		for (const list of document.findAll(".m-items-list")) {
-		// 			new MutationObserver(() => removeConfirmation(list)).observe(list, { childList: true, subtree: true });
-		// 		}
-		// 		break;
-		// }
+	async function startFeature() {
+		await requireElement(".users-point-sell");
+
+		removeConfirmation();
 	}
 
-	function removeConfirmation(source = document) {
-		// const isItemView = getViewType() === VIEW_TYPES.ITEM;
-		//
-		// for (const item of source.findAll(".items > li:not(.clear):not(.private-bazaar)")) {
-		// 	const icon = item.find(".buy .buy-link");
-		// 	if (!icon) continue;
-		//
-		// 	icon.dataset.action = "buyItemConfirm";
-		// 	icon.classList.add("yes-buy", "tt-modified");
-		//
-		// 	if (isItemView) icon.dataset.price = item.find(".cost").textContent.getNumber();
-		// }
-	}
-
-	function getViewType() {
-		const page = getHashParameters().get("p");
-
-		if (page === "shop") return VIEW_TYPES.ITEM;
-		else if (page === "market") return VIEW_TYPES.BROWSE;
-
-		return VIEW_TYPES.UNKNOWN;
+	function removeConfirmation() {
+		for (const item of document.findAll(".users-point-sell > li.bg-green:not(.yes) > span[href]")) {
+			item.classList.add("yes");
+			item.setAttribute("href", item.getAttribute("href").replace("ajax_action=remove", "ajax_action=remove1"));
+		}
 	}
 })();
